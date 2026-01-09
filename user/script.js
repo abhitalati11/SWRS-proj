4098b96e75a9069edd8046d1ff50c657cf87cc48
Abhi Talati <forfoxy027@gmail.com>
2026-01-09 14:58:19 +0530

    update 4

--- user/script.js
+++ user/script.js
@@ -31,20 +31,50 @@ wastes.forEach(waste => {
 const garbage = document.getElementById("garbage");
 const plastic = document.getElementById("plastic");
 const drainage = document.getElementById("drainage");
+const selectedarea = document.getElementById("area");
 const others = document.getElementById("others");
+const comments = document.getElementById("comments");
+let selection ;
+
+
 function runthis(){
   console.log("button clicked");
+  console.log(selection);
+  console.log(comments.value);
+  console.log(selectedarea.value);
+  checkUploadStatus();
 }
 
 garbage.onclick = function() {
-  console.log("Garbage selected");
+selection = "Garbage";
 }
+
 plastic.onclick = function() {
-  console.log("Plastic selected");
-} 
-drainage.onclick = function() {
-  console.log("Drainage selected");
+selection = "plastic";
 }
+
+drainage.onclick = function() {
+selection = "drainage";
+}
+
 others.onclick = function() {
-  console.log("Others selected");
-} 
+selection = "others";
+}
+
+function checkUploadStatus() {
+    // Get the input element
+    const input = document.getElementById('photoIP');
+    // Get the paragraph element to display the message
+    const statusMessage = document.getElementById('statusMessage');
+
+    // Check if the 'files' property exists and if its length is greater than 0
+    if (input.files && input.files.length > 0) {
+        // A file has been selected
+        statusMessage.textContent = 'A photo has been successfully selected for upload: ' + input.files[0].name;
+        statusMessage.style.color = 'green';
+    } else {
+        // No file has been selected
+        statusMessage.textContent = 'No photo selected yet.';
+        statusMessage.style.color = 'red';
+    }
+}
